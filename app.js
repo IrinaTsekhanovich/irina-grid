5cbb37d877b105c2251f47822d04ab9d1629729d
Irina Tsekhanovich <ira.tsekhanovich@yandex.ru>
2019-12-16 02:27:39 +0300

    cmmt_3
    I've finally fixed some stupied bugs.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT;
 const bodyParser = require('body-parser');
-const Auth0Strategy = require('passport-auth0');
 
 app.use(express.static('public'))
 
@@ -16,100 +15,6 @@ app.post('/post', (req, res) => {
     console.log(req.body);
     res.send('Success');
 })
-
-// Configure Passport to use Auth0
-const strategy = new Auth0Strategy(
-    {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENTID,
-        clientSecret: process.env.AUTH0_CLIENTID,
-        callbackURL:
-            process.env.AUTH0_CALLBACK_URL || '/callback'
-    },
-    function (accessToken, refreshToken, extraParams, profile, done) {
-        console.log(profile)
-        let userid = profile.id;
-        client.query('select count(client_id) as count from clients where client_id=$1',[userid], (err,res) => {
-            if (err) {
-                console.log(err.stack)
-            }
-
-            console.log(res.rows[0].count)
-            if (res.rows[0].count == 0) {
-                let lname = profile.name.familyName;
-                let fname = profile.name.givenName;
-                let ct = 1;
-                client.query('insert into clients (client_id,last_name,first_name,city_id) values ($1,$2,$3,$4)', [userid, lname, fname, ct], (err, res) => {
-                    if (err) {
-                        console.log(err.stack)
-                    }
-                })
-            }
-            })
-        // accessToken is the token to call Auth0 API (not needed in the most cases)
-        // extraParams.id_token has the JSON Web Token
-        // profile has all the information from the user
-        return done(null, profile)
-    }
-)
-passport.use(strategy);
-
-
-app.use(session(sess));
-app.use(passport.initialize());
-app.use(passport.session());
-//Внизу что-то интересное
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-    done(null, user);
-});
-
-app.get('/login', passport.authenticate('auth0', {
-    scope: 'openid email profile'
-}), function (req, res) {
-    res.redirect('/');
-});
-
-app.get('/callback', function (req, res, next) {
-    passport.authenticate('auth0', function (err, user, info) {
-        if (err) { return next(err); }
-        if (!user) { return res.redirect('/login'); }
-        req.logIn(user, function (err) {
-            if (err) { return next(err); }
-            const returnTo = req.session.returnTo;
-            delete req.session.returnTo;
-            res.redirect(returnTo || '/');
-        });
-    })(req, res, next);
-});
-
-app.get('/logout',(req,res)=>{
-    req.logout()
-    res.redirect('/')
-});
-
-
-
-app.get('/',(req, res) =>{
-    console.log(req.isAuthenticated())
-    res.sendFile(__dirname + '/public/index.html')
-})
-app.get('/index.html',(req, res) =>{
-    console.log(req.isAuthenticated())
-    res.sendFile(__dirname + '/public/index.html')
-});
-
-const auth = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        next()
-    }
-    else {
-        return res.redirect('/login')
-    }
-}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
--- public/client.js
+++ public/client.js
@@ -6,21 +6,3 @@ fetch("/post", {
         body: JSON.stringify({data: "Text"})
     }).then(res => res.text())
     .then(res => console.log(res));
-
-function getstatus(){
-    var xhr = new XMLHttpRequest();
-    console.log('ooijoijoij');
-    xhr.open('GET','/check',false);
-    xhr.onload = function(){
-        if (xhr.responseText == 'true') {
-            document.getElementById('log-in-btn').setAttribute('hidden','true');
-            document.getElementById('user-btn').removeAttribute('hidden');
-            console.log('yees');
-        }else{
-            document.getElementById('user-btn').setAttribute('hidden','true');
-            document.getElementById('log-in-btn').removeAttribute('hidden');
-            console.log('nooo');
-        }
-    };
-    xhr.send();
-}
